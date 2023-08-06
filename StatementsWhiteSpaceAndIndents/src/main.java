d08dbef960407e1d12f457093c34f35231f02a46
Nathan Thacker <nthacker878@gmail.com>
2023-08-06 17:04:15 -0500

    ver1.0

---
+++ StatementsWhiteSpaceAndIndents/src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    
+    public static void main(String[] args){
+
+        int myVariable = 50; //Expression myVariable = 50
+
+        myVariable++;   //statements
+        myVariable--;
+
+        System.out.println("This is a test"); // also statement
+
+        System.out.println("This is" + 
+                " another" + 
+                " still more."); // statement jshell
+    }
+}
