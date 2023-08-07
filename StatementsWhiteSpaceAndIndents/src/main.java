9255d49f5ef26e7357670c06ffea68b206ce1f37
Nathan Thacker <nthacker878@gmail.com>
2023-08-07 16:17:07 -0500

    ver1.1

--- StatementsWhiteSpaceAndIndents/src/main.java
+++ StatementsWhiteSpaceAndIndents/src/main.java
@@ -1,16 +1,19 @@
 public class main {
-    
-    public static void main(String[] args){
 
-        int myVariable = 50; //Expression myVariable = 50
+    public static void main(String[] args) {
 
-        myVariable++;   //statements
-        myVariable--;
+        int myVariable = 50; // Expression myVariable = 50
+
+        myVariable++; // statements
+        myVariable--; // java ignores white space
 
         System.out.println("This is a test"); // also statement
 
-        System.out.println("This is" + 
-                " another" + 
+        System.out.println("This is" +
+                " another" +
                 " still more."); // statement jshell
+
+        int anotherVariable = 50; myVariable--; System.out.println("myVariable = " + myVariable); // valid statement
+        
     }
 }
