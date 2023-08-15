200e7dbd2c45812ba1b5f36de3d4538348613e03
Nathan Thacker <nthacker878@gmail.com>
2023-08-15 17:53:09 -0500

    ver2.1

--- IfKeyWordAndCodeBlocks/src/mainChallenge.java
+++ IfKeyWordAndCodeBlocks/src/mainChallenge.java
@@ -1,14 +1,25 @@
 public class mainChallenge{    
     public static void main(String[] args){
 
-        calculateScore(true,800,5,100);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true,10000,8,200);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highscore is " + highScore);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        System.out.println("The Next highScore is " + 
+            calculateScore(gameOver, highScore, levelCompleted, bonus));
         }
     
 
     // New method must be outside other method but inside class
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
         int finalScore = score;
 
@@ -17,5 +28,7 @@ public class mainChallenge{
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
+
+        return finalScore;
     }
 }
